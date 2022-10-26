0209feb76e3446e991a07458cf6fe84bd6ec91ce
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-26 17:27:52 +0200

    Aggiunta classe alla prima immagine

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05jpg"];
+const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-let items = document.querySelector(`items`)
+let items = document.querySelector(`.items`)
 
 for (let i = 0; i < images.length; i++) {
     items.innerHTML +=`<div class="item"><img src="${images[i]}"></div>`;
