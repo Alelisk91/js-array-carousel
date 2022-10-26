cc652385fa3d40e61e28cef8a924ffd408d89e16
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-26 17:09:58 +0200

    Aggiunto classe alla prima foto

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05jpg"];
+
+let items = document.querySelector(`items`)
+
+for (let i = 0; i < images.length; i++) {
+    items.innerHTML +=`<div class="item"><img src="${images[i]}"></div>`;
+}
+
+document.querySelector('.item').classList.add('active')
+let active=0; 
